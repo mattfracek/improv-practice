627fe01ee6240670cd98cef52b39003a5adcfeb0
mattfracek <mattfracek@gmail.com>
2026-03-03 09:18:52 -0600

    Initial deploy

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'improv-v1';
+const FILES = ['/practice-app.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
